16ae70d354679758a84251bc7c985a2c3886a118
Paulo <98109654+Paulinho19@users.noreply.github.com>
2023-04-03 09:50:23 -0300

    insercao de estilos

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -3,15 +3,15 @@ type SearchProps = {
 }
 
 import { useState } from 'react'
-
 import {BsSearch} from 'react-icons/bs'
+import classes from './Search.module.css'
 
 const Search = ({loadUser}: SearchProps) => {
 
   const [userName, setUserName] = useState("")
   
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Search for github's user</h2>
       <p>Best repositories</p>
       <div>
